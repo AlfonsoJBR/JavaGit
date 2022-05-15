6120344ee82913d282c3d1697f04ad7ca70cf2a3
AlfonsoJBR <alfonsojesus.bravo.fp@iescampanillas.com>
2022-05-15 12:56:42 +0200

    Initial commit of project

---
+++ JavaGit/src/inicial/HolaMundo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package inicial;
+
+/**
+ * @author Alfonso Bravo Romo
+ * Este es un fichero para  un trabajo de la asignatura Entornos de Desarrollo 
+ * donde vamos a configurar Eclipse para trabajar con Git y Github  
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primera fase de nuestro super-programa");
+		System.out.println("Segunda fase de nuestro super-programa, cambiamos el fichero para modificarlo en nuestra staged area");
+	}
+
+}
