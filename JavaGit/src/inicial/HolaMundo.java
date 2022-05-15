a8e54049e62209511c6a5ad3890a73ad1d58e81f
AlfonsoJBR <alfonsojesus.bravo.fp@iescampanillas.com>
2022-05-15 14:36:26 +0200

    Ultima version de nuestro super-programa

--- JavaGit/src/inicial/HolaMundo.java
+++ JavaGit/src/inicial/HolaMundo.java
@@ -19,7 +19,8 @@ public class HolaMundo {
 		System.out.println("Primera fase de nuestro super-programa");
 		System.out.println("Segunda fase de nuestro super-programa, cambiamos el fichero para modificarlo en nuestra staged area");
 		System.out.println("Tercera fase de nuestro super-programa, cambiamos el fichero para hacer un segundo commit en local");
-	
+		System.out.println("Ultimo cambio en el super-programa");
+
 	}
 
 }
